3813cd8ae9ae6a7065e109b26e44f0bdd3ec682e
NadineBestard <nadine.bestard@ed.ac.uk>
2022-03-23 17:58:25 +0000

    add relative path to scripts

--- src/submit_noin.sh
+++ src/submit_noin.sh
@@ -14,16 +14,16 @@ if [[ $n -eq 1 ]]; then
  # the data is already in
 #   qsub -N stagein_"$n" stagein.sh $SAMPLE
 #   qsub -N cellranger_"$n" -hold_jid stagein_"$n" runCellRanger.sh $SAMPLE
-    qsub -N cellranger_"$n" runCellRanger.sh $SAMPLE    
-    qsub -N stageout_"$n" -hold_jid cellranger_"$n" stageout.sh $SAMPLE
+    qsub -N cellranger_"$n" src/runCellRangerMulti.sh $SAMPLE    
+    qsub -N stageout_"$n" -hold_jid cellranger_"$n" src/stageout.sh $SAMPLE
 
   # for the following ones also wait until the previous one has stageout before stagin
 else
 # data is already in
 #   qsub -N stagein_"$n" -hold_jid stageout_"$((n-1))" stagein.sh $SAMPLE
 #   qsub -N cellranger_"$n" -hold_jid stagein_"$n" runCellRanger.sh $SAMPLE
-    qsub -N cellranger_"$n" -hold_jid stageout_"$((n-1))" runCellRanger.sh $SAMPLE
-    qsub -N stageout_"$n" -hold_jid cellranger_"$n" stageout.sh $SAMPLE
+    qsub -N cellranger_"$n" -hold_jid stageout_"$((n-1))" src/runCellRangerMulti.sh $SAMPLE
+    qsub -N stageout_"$n" -hold_jid cellranger_"$n" src/stageout.sh $SAMPLE
   
 fi
 
