17d1798e69e15cd7428f11bf3f9760ac14caf119
NadineBestard <nadine.bestard@ed.ac.uk>
2022-03-25 12:53:30 +0000

    Test works, careful low mem

--- src/runCellRangerMulti.sh
+++ src/runCellRangerMulti.sh
@@ -2,15 +2,23 @@
 # options calculated from cellranger web:
 # https://support.10xgenomics.com/single-cell-gene-expression/software/overview/system-requirements
 # 128 total mem.
- 
-#$ -l h_rt=20:00:00
-#$ -l h_vmem=8G
-#$ -pe sharedmem 16
+#$ -cwd
+#$ -l h_rt=00:01:00
+#$ -l h_vmem=4G
+##$ -pe sharedmem 16
+
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+module load igmm/apps/cellranger/6.0.2
+# check if this is the Cellranger version you would like to use. 
+
 
 # cellranger multi
 
 # variables
 SAMPLE=$1 # from the submit script TK_$i
-
+csv=/exports/eddie/scratch/$USER/TK/src/config-files/config_${SAMPLE}.csv
 # run cellranger
-cellrnager multi --id=$SAMPLE --csv=config-files/config_$SAMPLE
+cellranger multi --id=$SAMPLE --csv=$csv
