567ffb18c2b44d82786a83b8109982d8d9c56024
NadineBestard <nadine.bestard@ed.ac.uk>
2022-05-27 12:55:58 +0100

    add run 3 and 4

--- src/renameFiles.sh
+++ src/renameFiles.sh
@@ -1,11 +1,34 @@
-# The samples were run in two different flow cells ( two days). 
-# We rename the samples from the second run, in order to differentiate them 
+# The samples were run in different flow cells ( different days). 
+# We rename the samples, in order to differentiate them 
 # in the config file for cellranger multi 
 #( email exchange with support: [10x Genomics] Cellranger multi with multiple flow cells)
 
+# variable
+SCRATCH=/exports/eddie/scratch/nbestard/
+# second run
 cd $SCRATCH/TK/data/raw_data/2022-03-21/TK_140322/outs/fastq_path/AAAL35KHV/
 for file in *.fastq.gz
 do
+  echo $file
   mv ${file} Run2_${file}
 
 done
+
+# third run
+cd $SCRATCH/TK/data/raw_data/2022-05-03/TK_280422/outs/fastq_path/AAAM5MVHV/
+   
+for file in *.fastq.gz
+do
+  echo $file
+  mv ${file} Run3_${file}
+done
+
+# fourth run 
+
+cd $SCRATCH/TK/data/raw_data/2022-05-20/TK_190522/outs/fastq_path/AAAMGTYHV/
+for file in *.fastq.gz
+do
+  echo $file
+  mv ${file} Run4_${file}
+done
+
