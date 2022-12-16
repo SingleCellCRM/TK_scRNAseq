28cc3ad47dceb3032f8ea0ff9a54cc0b18e459dd
NadineBestard <nadine.bestard@ed.ac.uk>
2022-12-16 12:35:02 +0000

    update main and shiny

--- src/R/generate-shinyapp.R
+++ src/R/generate-shinyapp.R
@@ -1,27 +1,40 @@
 library(ShinyCell)
 library(here)
+library(SingleCellExperiment) # access the coldata
 
 # project
 project <- "TK_all"
 # import dataset
 sce <- readRDS(here("processed", project,  "sce_labels.RDS"))
+
 #import colours
-source(here("src","colours.R"))
+source(here("src","R", "colours.R"))
 
 # configruation
 scconf <- createConfig(sce)
 
 # define colours and change them
-metadata_categorical <- c("lamanno")
+# clusters 
+srt_res <- grep("res",names(colData(sce)), value = TRUE)
+
+
+metadata_categorical <- c("lamanno", "Sample","ShortName", srt_res)
 for(metadata in metadata_categorical){
   # transform to factor
   sce[[metadata]] <- droplevels(as.factor(sce[[metadata]]))
   # replace colours ( need to specify the exact n of colours)
   length <- length(levels((sce[[metadata]])))
-  conf <- modColours(conf, meta.to.mod = metadata,
+  scconf <- modColours(scconf, meta.to.mod = metadata,
                      new.colours = cols[1:length])
 }
 
+scconf <- delMeta(scconf, meta.to.del = c( "subsets_mt_sum", "subsets_mt_detected",
+                                               "outlier", "discard", "total"))
+# modify metadata names
+scconf <- modMetaName(scconf,
+                    meta.to.mod = c("sum", "detected", "subsets_mt_percent", srt_res),
+                    new.name = c("umi counts", "detected genes", "% mt genes", paste("cluster", srt_res))
+)
 
 # make the shiny app
 makeShinyApp(sce, scconf, 
